9b2ea3a16526aa8e4a7035725595cb954a45ed98
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-11-30 10:25:39 +0100

    Add first java proekt

---
+++ src/zadaca11/deliviso3.java
@@ -0,0 +1,17 @@
+package zadaca11;
+import java.util.Scanner;
+public class deliviso3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		int i,n;
+		System.out.println("Do brojot");
+		n=input.nextInt();
+		i=1;
+		while(i<=n);
+		 
+		
+	}
+
+}
